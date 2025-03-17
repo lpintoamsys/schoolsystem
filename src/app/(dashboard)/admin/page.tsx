a619960a10f966cd2fe7b2110f708a35fbb24e6b
Lawrence Pinto <lpinto1204@gmail.com>
2025-03-17 14:06:08 +0530

    first commit

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,22 @@
+import UserCard from "@/components/UserCard";
+
 const AdminPage = () => {
     return (
-        <div>Admin Landing Page</div>
+        <div className="p-4 flex gap-4 flex-col md:flex-row">
+            {/*LEFT */}
+            <div className="w-full lg:w-2/3">
+                {/* User Cards*/}
+                <div className="flex gap-4 justify-between flex-wrap">
+                    <UserCard type="students" />
+                    <UserCard type="teachers" />
+                    <UserCard type="parents"/>
+                    <UserCard type="staffs"/>
+                </div>
+            </div>
+
+            {/* RIGHT */}
+            <div className="w-full lg:w-1/3">R</div>
+        </div>
     )
 };
 
